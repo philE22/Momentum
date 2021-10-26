2e4a8588dad7eabc8ac6fd82feb903850a28f92d
philE22 <moonwhlking3@naver.com>
2021-10-26 22:35:37 +0900

    ~#4
    4강 까지의 코드
    login form

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert("Hi");
+const onLoginForm = document.querySelector("#login-form")
+const onLoginInput = document.querySelector("#login-form input");
+const onLoginGreeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault(); 
+    const username = onLoginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    onLoginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreeting(username);
+}
+
+function paintGreeting(username){
+    onLoginGreeting.innerText = `Hello ${username}!`;
+    onLoginGreeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null){
+    onLoginForm.classList.remove(HIDDEN_CLASSNAME);
+    onLoginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
