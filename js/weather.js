1cb1754c335312365c12d328f7251c8fdfda7c92
philE22 <moonwhlking3@naver.com>
2021-10-31 23:53:26 +0900

    ㅇㅇ
    ㅇㅇ

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,6 @@ function onGeoOk(position){
     console.log(position)
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat, lon)
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then((response)=> response.json()).then((data)=>{
         const weather = document.querySelector(".weather span:first-child")
